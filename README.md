9a9037cf16ebfd2ce90540b7a547f367a6b0f45d
neil-oliver <neiloliverphoto@gmail.com>
2021-06-23 13:25:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Fivetran x Data Culture
+*Fivetran Custom Connector Template*
+
+This repo is part of the [Fivetran x Data Culture collaboration event](https://www.eventbrite.com/e/lunch-learn-continuous-data-ingestion-using-fivetran-custom-connectors-tickets-159677649147) on Continuous Data Ingestion using Custom Connectors.
+
+This is an example function written in [node.js](https://nodejs.org/en/) for use with Fivetran's custom connector feature.
+The example uses the [Wikipedia API](https://en.wikipedia.org/w/api.php) to call an initial dataset and maintain continuous updates; however the template is designed to be easily adaptable for any data source including both REST and GraphQL types.
+
+## Fivetran Emulator
+The Fivetran emulator function is designed to replicate the Fivetran custom connector on a *basic* level. No ingestion is passed to Fivetran through this emulator and no scheduling takes place. It can be used to observe the output (via console logs) of the custom function and test the ```hasMore``` feature. The emulator can be used to save data to a ```.CSV``` file for data quality assurance checks. 
+
+## Links
+[Fivetran Custom Connector Setup](https://fivetran.com/docs/functions)
+[Data Culture](https://www.datacult.com/)
+[Fivetran](https://fivetran.com/)
+
+## Notes
+- ```index.js``` filename can be changed.
+- AWS Lambda requires a zip file including node_modules folder
+- Google Cloud Functions will install dependencies from package.json
---
+++ index.js
@@ -0,0 +1,122 @@
+/*
+Axios is a simple HTTP request package which can be easily adapted to call most REST OR GraphQL endpoints.
+For more complex APIs there may be additional libraries in the npm package library (https://www.npmjs.com/) that may be helpful.
+*/
+const axios = require('axios');
+
+// lambda entry point for the function
+exports.handler = async (request, context, callback) => {
+    callback(null, await update(request.state, request.secrets));
+};
+
+/*
+// Google Cloud Function entry point
+exports.handler = async (req, res) => {
+    if (req.body.state === undefined) res.status(400).send('No state is defined!');
+    
+    if (req.body.secrets === undefined) res.status(400).send('No secrets is defined!');
+        
+    res.header("Content-Type", "application/json");	   
+
+    res.status(200).send(await update(req.body.state, req.body.secrets));
+};
+*/
+
+
+// Builds the data into the FiveTran required format
+async function update(state, secrets) {
+
+    // Note: 'logevents' is the table name and can be changed. multiple tables can be updated in the same call
+
+    let fivetran_structure = {
+        state: {},
+        insert: {
+            logevents: [],
+        },
+        delete: {},
+        schema: {
+            logevents: {
+                primary_key: ['logid']
+            },
+        },
+        hasMore: false
+    }
+
+    // fill out any default state for initial call
+    if (Object.keys(state).length == 0) {
+        state = {
+            last_updated: new Date(0).toISOString // if no last_update timestamp is provided, start from 01/01/1970
+        }
+    }
+
+
+    // Fetch records using api calls
+    let [entries, stateUpdate, more] = await apiResponse(state, secrets);
+
+    // populate Fivetran structure
+    fivetran_structure.state = stateUpdate
+    fivetran_structure.insert.projects = entries
+    fivetran_structure.hasMore = more
+
+    return (fivetran_structure);
+
+}
+
+async function apiResponse(state, secrets) {
+
+    // params from https://en.wikipedia.org/w/api.php?action=help&modules=query%2Blogevents
+    const params = {
+        action: 'query',
+        list: 'logevents', // type of data to return from the API
+        title: 'Data', // title or wikipedia article to get the log of updates for
+        lestart: state.last_updated, // return only results after this date
+        lelimit: 10, // limit how many results to get any any one time
+        format: 'json' // format of data to return from the API
+    }
+
+    /*
+    --- API AUTHENTICATION ---
+
+    This is an example of how to pass user authentication to an API using Axios.
+    Any secret keys should be passed through the secrets object from Fivetran (see BASE_URL in this example).
+    The wikipedia API endpoints in this example do not need authentication.
+    
+    const headers = {
+        'API_KEY': secrets.API_KEY
+    }
+
+    const auth = { username: secrets.USERNAME, password: secrets.PASSWORD }
+    */
+
+    try {
+        // make request to API
+        const response = await axios.get(secrets.BASE_URL, {
+            params: params,
+            // headers: headers,
+            // auth: auth
+        });
+
+        let entries = []
+
+        // check that the response is not empty
+        if (response.data.length > 0) {
+            
+            response.data.query.logevents.forEach(d => {
+                // any required data manipulation can go here before pushing to data array
+                data.push(d)
+            })
+        }
+
+        return [
+            entries,
+            state, // updated state
+            false // hasMore flag
+        ];
+
+    } catch (error) {
+        // If the axios call fails, an error is thrown. These console log messages will appear in the AWS Lamdba or Google Cloud Function logs.
+        console.log("error: ", error);
+        console.log("state: ", state)
+    }
+
+}
